faec415cf7d93df666cec96e758fdac5040be7b9
alexisabels <isabelsanchezalex@gmail.com>
2025-02-12 23:43:27 +0100

    preparing PWA support v0

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["gas.svg"],
+      manifest: {
+        name: "Precios de Combustible",
+        short_name: "Carburantes",
+        description:
+          "Encuentra los mejores precios de combustible cerca de ti.",
+        background_color: "#f0f2f5",
+        theme_color: "#2c3e50",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
