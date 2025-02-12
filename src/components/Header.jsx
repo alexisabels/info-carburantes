e91213ebba89d7b1bcf40c87a9abf432f78cc259
alexisabels <isabelsanchezalex@gmail.com>
2025-02-13 00:26:42 +0100

    Update header layout and styles; add responsive name display and adjust logo alignment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
-
+import "./Header.css";
 const Header = () => (
   <header className="header">
     <div className="header-content">
       <div className="logo">
         <img src="/gas.svg" alt="Logo" />
         <Link to="/" className="logo-a">
-          Comparador de Combustible
+          <span className="name-large">Comparador de Combustible</span>
+          <span className="name-small">Carburantes</span>
         </Link>
       </div>
       <nav className="navbar">
